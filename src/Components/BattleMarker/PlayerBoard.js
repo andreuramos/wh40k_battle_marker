5f6315792f8022c0a69d9b9baf4c364996441b64
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-09-15 15:11:47 +0200

    show command points

--- src/Components/BattleMarker/PlayerBoard.js
+++ src/Components/BattleMarker/PlayerBoard.js
@@ -29,7 +29,10 @@ class PlayerBoard extends React.Component
                 <div className="player-board-body">
                     <div className="player-board-score">
                         <div className="player-board-score-trophy">
-                            <span className="player-board-score-points">{this.state.player.score()}</span>
+                            <span className="player-board-points">{this.state.player.score()}</span>
+                        </div>
+                        <div className='player-board-command-points'>
+                            <span className="player-board-points">{this.state.player.commandPoints()}</span>
                         </div>
                     </div>
                     <div className="player-board-missions">
