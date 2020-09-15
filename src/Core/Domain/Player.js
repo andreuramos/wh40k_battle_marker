dbe8b41f3d48cfc4cbb84bb15a3693dab3095d47
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-09-15 09:08:01 +0200

    added CP management to player

--- src/Core/Domain/Player.js
+++ src/Core/Domain/Player.js
@@ -3,12 +3,14 @@ class Player
     _name: String;
     _score: Number;
     _completedMissions: Array
+    _commandPoints: Number;
 
     constructor(name)
     {
         this._name = name;
         this._score = 0;
-        this._completedMissions = []
+        this._completedMissions = [];
+        this._commandPoints = 0;
     }
 
     name(): String
@@ -35,6 +37,23 @@ class Player
     {
         this._completedMissions.push(mission)
         this.addScore(mission.points())
+    }
+
+    commandPoints(): Number
+    {
+        return this._commandPoints;
+    }
+
+    addCommandPoints(points): void
+    {
+        this._commandPoints += points;
+    }
+
+    substractCommandPoints(points): void
+    {
+        if (this._commandPoints - points >= 0) {
+            this._commandPoints -= points;
+        }
     }
 }
 
--- src/Core/Domain/Player.spec.js
+++ src/Core/Domain/Player.spec.js
@@ -3,26 +3,48 @@ import Player from "./Player";
 import CompletedMission from "./CompletedMission";
 
 describe('Player', () => {
+    let player;
+
+    beforeEach(() => {
+        player = new Player("Andreu")
+    })
+
     it('sets the name when created', () => {
-        const player = new Player("Andreu")
         expect(player.name()).toBe("Andreu")
     })
 
     it('increments score', () => {
-        const player = new Player("Andreu")
-
         player.addScore(2);
 
         expect(player.score()).toBe(2);
     })
 
     it('adds points to score when adding a mission', () => {
-        const player = new Player("Andreu");
         const mission = new CompletedMission("Domination", 3);
 
         player.addCompletedMission(mission)
 
         expect(player.score()).toBe(3)
         expect(player.completedMissions().length).toBe(1)
+    })
+
+    it('increases command points', () => {
+        player.addCommandPoints(1)
+
+        expect(player.commandPoints()).toBe(1)
+    })
+
+    it('decreases command points', () => {
+        player.addCommandPoints(3)
+        player.substractCommandPoints(1)
+
+        expect(player.commandPoints()).toBe(2)
+    })
+
+    it('does not decrease command points below 0', () => {
+        player.addCommandPoints(1)
+        player.substractCommandPoints(2)
+
+        expect(player.commandPoints()).toBe(1)
     })
 })
