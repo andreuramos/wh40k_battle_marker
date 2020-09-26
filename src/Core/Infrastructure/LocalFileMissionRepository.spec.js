ca43735a202fedd98ac9886d564d65fe136c4c59
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-09-26 14:17:03 +0200

    splits mission repo tests

--- src/Core/Infrastructure/LocalFileMissionRepository.spec.js
+++ src/Core/Infrastructure/LocalFileMissionRepository.spec.js
@@ -2,12 +2,20 @@ import React from "react";
 import LocalFileMissionRepository from "./LocalFileMissionRepository";
 
 describe("LocalFileMissionRepository", () => {
-    it("gets all missions from file", () => {
+    it("returns an array of Missions", () => {
         const repo = new LocalFileMissionRepository('./testmissions.json')
 
         const missions = repo.getAllMissions()
 
         expect(Array.isArray(missions)).toBe(true)
-        expect(missions.length).toBe(2)
+        expect(missions[0].constructor.name).toBe("Mission");
     });
+
+    it("gets all the missions", () => {
+        const repo = new LocalFileMissionRepository('./testmissions.json')
+
+        const missions = repo.getAllMissions()
+
+        expect(missions.length).toBe(2)
+    })
 });
