00d47b678748844c3976b6bd52a4f63cc11f09f6
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-10-06 21:13:58 +0200

    removed unnecessary console logs

--- src/Core/Domain/MissionBuilder.js
+++ src/Core/Domain/MissionBuilder.js
@@ -7,11 +7,6 @@ class MissionBuilder
     {
         const objectives_repo = new LocalFileObjectiveRepository()
 
-        console.log("main objective")
-        const obj = objectives_repo.findByKey(element['main_objective'])
-        console.log(obj)
-        console.log(obj.description())
-
         return new Mission(
             element['name'],
             element['description'],
