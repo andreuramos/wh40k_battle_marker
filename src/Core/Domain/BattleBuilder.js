66693e540203e95bc859cfa8b3d029933922d828
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-10-10 21:59:53 +0200

    build battle - secondary objectives

--- src/Core/Domain/BattleBuilder.js
+++ src/Core/Domain/BattleBuilder.js
@@ -4,11 +4,25 @@ import Battle from "./Battle";
 export default class BattleBuilder
 {
     static fromForm(data) {
-        let player1 = new Player(data.player1)
-        let player2 = new Player(data.player2)
+        const player1 = new Player(data.player1)
+        const player2 = new Player(data.player2)
 
         player1.addObjective(data.mission.mainObjective())
         player2.addObjective(data.mission.mainObjective())
+
+        for (let key in data.player1_objectives) {
+            let objective = data.player1_objectives[key]
+            if (objective) {
+                player1.addObjective(objective)
+            }
+        }
+
+        for (let key in data.player2_objectives) {
+            let objective = data.player1_objectives[key]
+            if (objective) {
+                player2.addObjective(objective)
+            }
+        }
 
         if (data.startingPlayer !== data.player1) {
             return new Battle(player2, player1)
--- src/Core/Domain/BattleBuilder.spec.js
+++ src/Core/Domain/BattleBuilder.spec.js
@@ -7,11 +7,18 @@ describe('BattleBuilder', () => {
     const objective = new Objective("TestMain","test-main", "desc", "main", "main")
     const secondary = new Objective("TestSecondary", "test-sec", "desc", "secondary", "main")
     const mission = new Mission("Test", "test test", objective, secondary, "test", "any")
-    let form_data = {
+    const secondary_objectives = [
+        new Objective("Secondary1", "sec1", "desc", "secondary", "cat1"),
+        new Objective("Secondary2", "sec2", "desc", "secondary", "cat2"),
+        new Objective("Secondary3", "sec3", "desc", "secondary", "cat3")
+    ]
+    const form_data = {
         player1: "Andreu",
         player2: "Pedro",
         startingPlayer: "Andreu",
-        mission: mission
+        mission: mission,
+        player1_objectives: secondary_objectives,
+        player2_objectives: secondary_objectives
     }
 
     it('creates the battle with the player\'s names', () => {
@@ -22,9 +29,10 @@ describe('BattleBuilder', () => {
     })
 
     it('sets starting player as player 1', () => {
-        form_data.startingPlayer = "Pedro"
+        let data = { ...form_data}
+        data.startingPlayer = "Pedro"
 
-        const battle = BattleBuilder.fromForm(form_data)
+        const battle = BattleBuilder.fromForm(data)
 
         expect(battle.player1().name()).toBe("Pedro")
         expect(battle.player2().name()).toBe("Andreu")
@@ -35,5 +43,36 @@ describe('BattleBuilder', () => {
 
         expect(battle.player1().objectives()[0]).toBe(objective)
         expect(battle.player2().objectives()[0]).toBe(objective)
+    })
+
+    it('sets empty secondary objectives', () => {
+        let data = { ...form_data}
+        data.player1_objectives = []
+        data.player2_objectives = []
+
+        const battle = BattleBuilder.fromForm(data)
+
+        expect(battle.player1().objectives().length).toBe(1)
+        expect(battle.player2().objectives().length).toBe(1)
+    })
+
+    it('sets non empty secondary objectives', () => {
+        const battle1 = BattleBuilder.fromForm(form_data)
+
+        expect(battle1.player1().objectives().length).toBe(4)
+        expect(battle1.player2().objectives().length).toBe(4)
+    })
+
+    it('does not set null objectives', () => {
+        let data = { ...form_data }
+        data.player1_objectives = [
+            new Objective("Only", "one", "objective", "secondary", "cat"),
+            null,
+            null
+        ]
+
+        const battle1 = BattleBuilder.fromForm(data)
+
+        expect(battle1.player1().objectives().length).toBe(2)
     })
 })
