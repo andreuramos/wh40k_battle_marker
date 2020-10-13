8f3ea9cd1c6fabe15a0fd473aa87bd50913bb2e1
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-10-13 20:40:57 +0200

    validation of players names

--- src/Components/BattleForm/PlayersNameForm.js
+++ src/Components/BattleForm/PlayersNameForm.js
@@ -8,15 +8,26 @@ class PlayersNameForm extends React.Component
         super(props);
         this.state = {
             player1: '',
-            player2: ''
+            player2: '',
+            errors: {}
         }
     }
 
     handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
+        this.setState({
+            [event.target.name]: event.target.value,
+            errors: {}
+        })
     }
 
     submitForm = () => {
+
+        const errors = this.validate()
+        if(errors.player1 || errors.player2) {
+            this.setState({errors: errors})
+            return
+        }
+
         if (this.props.onSubmit) {
             const data = {
                 player1: this.state.player1,
@@ -26,6 +37,20 @@ class PlayersNameForm extends React.Component
         }
     }
 
+    validate = () => {
+        let errors = {}
+        if (this.state.player1.length < 3) {
+            errors.player1 = "Name is too short"
+        }
+        if (this.state.player2.length < 3) {
+            errors.player2 = "Name is too short"
+        }
+        if (this.state.player1 === this.state.player2) {
+            errors.player2 = "Players cannot have the same name"
+        }
+        return errors;
+    }
+
     render() {
         return (
             <div>
@@ -33,25 +58,31 @@ class PlayersNameForm extends React.Component
                     <div className="input-group">
                         <label htmlFor="player1">Player 1</label>
                         <input
-                            className="text-input"
+                            className={this.state.errors.player1 ? "text-input error" : "text-input" }
                             type="text"
                             name="player1"
                             autoComplete="off"
                             value={this.state.player1}
                             onChange={this.handleChange}
                         />
+                        { this.state.errors.player1 ?
+                            <span className="error-message">{this.state.errors.player1}</span> : ""
+                        }
                     </div>
 
                     <div className="input-group">
                         <label htmlFor="player2">Player 2</label>
                         <input
-                            className="text-input"
+                            className={this.state.errors.player2 ? "text-input error" : "text-input"}
                             type="text"
                             name="player2"
                             autoComplete="off"
                             value={this.state.player2}
                             onChange={this.handleChange}
                         />
+                        { this.state.errors.player2 ?
+                            <span className="error-message">{this.state.errors.player2}</span> : ""
+                        }
                     </div>
                 </form>
                 <Button id="submit-player-form" text="Next" onClick={this.submitForm} />
--- src/Components/BattleForm/PlayersNameForm.spec.js
+++ src/Components/BattleForm/PlayersNameForm.spec.js
@@ -10,15 +10,34 @@ describe("Players Name Form", () => {
 
     it('executes onsubmit prop when submit button is clicked', () => {
         const submitfn = jest.fn();
+        const component = shallow(<PlayersNameForm onSubmit={submitfn}/>);
 
-        const component = shallow((<PlayersNameForm onSubmit={submitfn}/>));
-        component.find('[name="player1"]').simulate('change', {target: {value: "Andreu"}})
-        component.find('[name="player2"]').simulate('change', {target: {value: "Andreu"}})
+        component.find('[name="player1"]').simulate('change', {target: {value: "Andreu", name: "player1"}})
+        component.find('[name="player2"]').simulate('change', {target: {value: "Ramos", name: "player2"}})
         component.find('#submit-player-form').simulate('click');
-        expect(submitfn.mock.calls.length).toEqual(1);
+
+        expect(submitfn.mock.calls.length).toBe(1);
     })
 
-    it('does not execute onSubmit if validation fails', () => {
+    it('does not execute onSubmit if names are too short', () => {
+        const submitfn = jest.fn();
+        const component = shallow(<PlayersNameForm onSubmit={submitfn}/>)
 
+        component.find('[name="player1"]').simulate('change', {target: {value: "an", name: "player1"}})
+        component.find('[name="player2"]').simulate('change', {target: {value: "ra", name: "player2"}})
+        component.find('#submit-player-form').simulate('click')
+
+        expect(submitfn.mock.calls.length).toBe(0)
+    })
+
+    it('does not submit if both names are the same', () => {
+        const submitfn = jest.fn();
+        const component = shallow(<PlayersNameForm onSubmit={submitfn}/>)
+
+        component.find('[name="player1"]').simulate('change', {target: {value: "andreu", name: "player1"}})
+        component.find('[name="player2"]').simulate('change', {target: {value: "andreu", name: "player2"}})
+        component.find('#submit-player-form').simulate('click')
+
+        expect(submitfn.mock.calls.length).toBe(0)
     })
 })
